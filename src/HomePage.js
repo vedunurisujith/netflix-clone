9a0d58517942bf2ca07c37324ab71bfa3eb77d1e
vedunurisujith <vedunurisujith123@gmail.com>
2023-06-01 13:18:31 +0530

    initial commit

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import "./HomePage.css";
+import NavBar from "./NavBar";
+import Slider from "./Slider";
+
+function HomePage() {
+  return (
+    <div className="home_box">
+      <NavBar />
+      <Slider />
+    </div>
+  );
+}
+export default HomePage;
---
+++ src/myList.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+
+const myList = () =>{
+
+    return <div className="list-home">
+        <div className="list-cont">
+        </div>
+    </div>
+
+}
+
+export default myList;
